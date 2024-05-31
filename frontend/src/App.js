71d25d72afc3a59f80a432016d99f452c2a410eb
kzau1612 <ngohoangkim2002@gmail.com>
2024-05-31 16:09:36 +0700

    blog

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './page/home/Home';
-import { Link } from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/home/Home";
+import { Link } from "react-router-dom";
 
-import Footer from './compoment/general/Footer';
-import { Navigate } from 'react-router-dom';
-import About from './page/aboutme/About';
-
+import Footer from "./compoment/general/Footer";
+import { Navigate } from "react-router-dom";
+import About from "./page/aboutme/About";
+import Blog from "./page/blog/Blog";
 
 function App() {
-  
-
-
-
   return (
     <div>
       <BrowserRouter>
-     
-      <Routes>     
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/' element={<Navigate to="/home"></Navigate>} ></Route>
-        <Route path='/about' element={<About></About>}></Route>
-       
-      </Routes>
-      <Footer></Footer>
+        <Routes>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/blog" element={<Blog></Blog>}></Route>
+        </Routes>
+        <Footer></Footer>
       </BrowserRouter>
     </div>
   );
