26c0a5bc08727ca9bde207290ff05917c6741d62
Trần Trung Kiên <trungkien2981412@gmail.com>
2024-06-07 06:58:28 +0700

    done bmi

--- frontend/src/compoment/general/BmiCalculator.jsx
+++ frontend/src/compoment/general/BmiCalculator.jsx
@@ -5,7 +5,7 @@ const BmiCalculator = () => {
   const [height, setHeight] = useState(0);
   const [weight, setWeight] = useState(0);
   const [bmi, setBmi] = useState(0);
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,24 +15,24 @@ const BmiCalculator = () => {
 
   const getAdvice = (bmi) => {
     if (bmi < 18.5) {
-      return "You are underweight. It's important to eat a balanced diet and consult with a healthcare provider to determine the cause and address it appropriately.";
+      return "temp1";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-      return "You have a healthy weight. Keep up with your balanced diet and regular physical activity to maintain your weight.";
+      return "temp2";
     } else if (bmi >= 25 && bmi <= 29.9) {
-      return "You are overweight. Consider adopting a healthier diet and increasing your physical activity. Consulting with a healthcare provider can also help you create a plan.";
+      return "temp3";
     } else {
-      return "You are obese. It's important to take action to reduce your weight through a healthy diet and regular exercise. Seeking advice from a healthcare provider is highly recommended.";
+      return "temp4";
     }
   };
 
   return (
     <div>
-      <div class="slider-area2">
-        <div class="slider-height2 d-flex align-items-center">
-          <div class="container">
-            <div class="row">
-              <div class="col-xl-12">
-                <div class="hero-cap hero-cap2 pt-70">
+      <div className="slider-area2">
+        <div className="slider-height2 d-flex align-items-center">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-12">
+                <div className="hero-cap hero-cap2 pt-70">
                   <h2>{t('BMI CALCULATOR')}</h2>
                 </div>
               </div>
@@ -41,14 +41,14 @@ const BmiCalculator = () => {
         </div>
       </div>
 
-      <section class="bmi-calculator-section spad">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="section-title chart-title">
+      <section className="bmi-calculator-section spad">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="section-title chart-title">
                 <h2>{t('CHART.name')}</h2>
               </div>
-              <div class="chart-table">
+              <div className="chart-table">
                 <table>
                   <thead>
                     <tr>
@@ -58,48 +58,48 @@ const BmiCalculator = () => {
                   </thead>
                   <tbody>
                     <tr>
-                      <td class="point">{t('CHART.Below 18.5')}</td>
+                      <td className="point">{t('CHART.Below 18.5')}</td>
                       <td>{t('CHART.Underweight')}</td>
                     </tr>
                     <tr>
-                      <td class="point">18.5 - 24.9</td>
+                      <td className="point">18.5 - 24.9</td>
                       <td>{t('CHART.Healthy')}</td>
                     </tr>
                     <tr>
-                      <td class="point">25.0 - 29.9</td>
+                      <td className="point">25.0 - 29.9</td>
                       <td>{t('CHART.Overweight')}</td>
                     </tr>
                     <tr>
-                      <td class="point">{t('CHART.30.0 - and Above')}</td>
+                      <td className="point">{t('CHART.30.0 - and Above')}</td>
                       <td>{t('CHART.Obesity')}</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
             </div>
-            <div class="col-lg-6">
-              <div class="section-title chart-calculate-title">
+            <div className="col-lg-6">
+              <div className="section-title chart-calculate-title">
                 <h2>{t('calculate.name')}</h2>
               </div>
-              <div class="chart-calculate-form">
-                <p>
-                {t('calculate.description')}
-                </p>
+              <div className="chart-calculate-form">
+                <p>{t('calculate.description')}</p>
                 <form onSubmit={handleSubmit}>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <input type="text" placeholder="Height / cm" onChange={(e) => setHeight(e.target.value)} />
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <input
+                        type="text"
+                        placeholder="Height / cm"
+                        onChange={(e) => setHeight(Number(e.target.value))}
+                      />
                     </div>
-                    <div class="col-sm-6">
-                      <input type="text" placeholder="Weight / kg" onChange={(e) => setWeight(e.target.value)} />
+                    <div className="col-sm-6">
+                      <input
+                        type="text"
+                        placeholder="Weight / kg"
+                        onChange={(e) => setWeight(Number(e.target.value))}
+                      />
                     </div>
-                    {/* <div class="col-sm-6">
-                      <input type="text" placeholder="Age" />
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="text" placeholder="Sex" />
-                    </div> */}
-                    <div class="col-lg-12">
+                    <div className="col-lg-12">
                       <button type="submit">{t('button')}</button>
                     </div>
                   </div>
@@ -107,7 +107,20 @@ const BmiCalculator = () => {
                 {bmi > 0 && (
                   <div>
                     <p>{t('calculate.total')} {bmi.toFixed(2)}</p>
-                    <p>{getAdvice(bmi)}</p>
+                    <p>
+                      {(() => {
+                        const advice = getAdvice(bmi);
+                        if (advice === "temp1") {
+                          return t('calculate.result.one');
+                        } else if (advice === "temp2") {
+                          return t('calculate.result.two');
+                        } else if (advice === "temp3") {
+                          return t('calculate.result.three');
+                        } else {
+                          return t('calculate.result.four');
+                        }
+                      })()}
+                    </p>
                   </div>
                 )}
               </div>
